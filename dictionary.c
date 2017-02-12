3f7f4b7723b2eb2b97b399f61fbd02c7bec79c0e
Rino Jose <rjose@spreadsheetkit.com>
2017-02-12 13:06:35 -0800

    Add documentation to dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -1,3 +1,19 @@
+// =============================================================================
+/** \file
+
+This defines the functions for manipulating the global Forth dictionary: _dictionary.
+*/
+// =============================================================================
+
+
+// -----------------------------------------------------------------------------
+/** Searches for an entry in the dictionary, starting from the most recent entry
+and moving backwards.
+
+\param word: The string to search for
+\returns A pointer to the entry or NULL if not found
+*/
+// -----------------------------------------------------------------------------
 Entry* find_entry(const gchar* word) {
     for (GList* l=g_list_last(_dictionary); l != NULL; l = l->prev) {
         Entry *entry = l->data;
@@ -7,7 +23,13 @@ Entry* find_entry(const gchar* word) {
 }
 
 
-// Allocates new entry, adds it to the dictionary, and returns it.
+// -----------------------------------------------------------------------------
+/** Allocates new Entry, adds it to the dictionary, and returns it.
+
+\param word: The word to use for the new entry 
+\returns The newly created entry
+*/
+// -----------------------------------------------------------------------------
 Entry *add_entry(const gchar *word) {
     Entry *result = g_new(Entry, 1);
     g_strlcpy(result->word, word, MAX_WORD_LEN);
@@ -17,10 +39,15 @@ Entry *add_entry(const gchar *word) {
 
 
 // -----------------------------------------------------------------------------
-// Builds initial dictionary
-//
-// NOTE: Entries are allocated and so it's appropriate to do a g_list_free_full
-//       if the dictionary should be rebuilt.
+/** Builds the dictionary for the interpreter.
+
+This defines the basic words for the interpreter and will allow loading
+of custom extensions for various applications. This is TBD, but the intent is
+that we can control the extensions dynamically.
+
+\note Entries are new'd and so it's appropriate to do a g_list_free_full
+      if the dictionary should be rebuilt.
+*/
 // -----------------------------------------------------------------------------
 void build_dictionary() {
     Entry *entry;
@@ -33,6 +60,10 @@ void build_dictionary() {
 }
 
 
+// -----------------------------------------------------------------------------
+/** Deallocates dictionary and all of its entries.
+*/
+// -----------------------------------------------------------------------------
 void destroy_dictionary() {
     g_list_free_full(_dictionary, free_entry);
 }
